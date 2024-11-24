7f5265ec6ce477e2334eb1fa5daba98e75c5f088
cagri.yolyapar <cagri.yolyapar@gmail.com>
2024-11-24 11:15:36 +0300

    API Crud işlemleri tamamlandı

--- CoreAPIDevam_1/Controllers/CategoryController.cs
+++ CoreAPIDevam_1/Controllers/CategoryController.cs
@@ -1,7 +1,10 @@
 ﻿using CoreAPIDevam_1.Models.ContextClasses;
+using CoreAPIDevam_1.Models.Entities;
+using CoreAPIDevam_1.Models.ViewModels.Categories.RequestModels;
 using CoreAPIDevam_1.Models.ViewModels.Categories.ResponseModels;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CoreAPIDevam_1.Controllers
 {
@@ -18,6 +21,9 @@ namespace CoreAPIDevam_1.Controllers
         }
 
 
+        //ID'ya göre Kategori getirme, Kategori ekleme, silme ve güncelleme
+
+
         [HttpGet]
         public IActionResult GetCategories()
         {
@@ -30,6 +36,63 @@ namespace CoreAPIDevam_1.Controllers
 
 
             return Ok(categories);
+        }
+
+        [HttpGet("GetById")]
+        public async Task<IActionResult> GetCategory(int id)
+        {
+            //Category c =await _db.Categories.FindAsync(id);
+            //CategoryResponseModel crm = new CategoryResponseModel()
+            //{
+            //    ID = c.ID,
+            //    CategoryName = c.CategoryName,
+            //    Description = c.Description
+            //};
+
+            CategoryResponseModel? crm = await _db.Categories.Where(x => x.ID == id).Select(x => new CategoryResponseModel
+            {
+                ID = x.ID,
+                CategoryName = x.CategoryName,
+                Description = x.Description
+            }).FirstOrDefaultAsync();
+
+            return Ok(crm);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddCategory(CreateCategoryRequestModel createcategory)
+        {
+
+
+
+
+            Category c = new()
+            {
+                CategoryName = createcategory.CategoryName,
+                Description = createcategory.Description
+            };
+            await _db.Categories.AddAsync(c);
+            await _db.SaveChangesAsync();
+            return Ok("Kategori eklendi");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateCategory(UpdateCategoryRequestModel updateCategory)
+        {
+            Category? c = await _db.Categories.FindAsync(updateCategory.ID);
+            c.CategoryName = updateCategory.CategoryName;
+            c.Description = updateCategory.Description;
+            await _db.SaveChangesAsync();
+            return Ok("Güncelleme basarılı");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteCategory(int id)
+        {
+            Category? c = await _db.Categories.FindAsync(id);
+            _db.Categories.Remove(c);
+            await _db.SaveChangesAsync();
+            return Ok("Silme işlemi basarılıdır");
         }
     }
 }
